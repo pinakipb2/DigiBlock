a4c16210017fbedfc74e9a6035a7c07d5f80eff6
grsakshi <sakshi.g642001@gmail.com>
2021-10-05 21:59:37 +0530

    added navbar, hero and info components

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,17 @@
 import React from 'react';
+import Hero from './components/Hero/Hero';
+import Info from './components/Info';
+import Navbar from './components/Navbar';
 
-const App = () => (
-  <div />
-);
+function App() {
+  return (
+    <div className="bg-white h-screen">
+      <Navbar />
+      <Hero />
+      <Info />
+      {/* 4 cards */}
+    </div>
+  );
+}
 
 export default App;
--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,7 +1,23 @@
 import React from 'react';
 
 const Navbar = () => (
-  <div />
+  <nav className="items-center">
+    <div className="bg-white w-full px-4 py-3 flex justify-around fixed z-50">
+      <div className="w-full flex justify-between items-center">
+        <div className="flex items-center">
+          <img src="assets/nav_logo.png" alt="logo" className="h-16 ml-4" />
+        </div>
+        <div className="flex-row">
+          <a href="signin" className="ml-2 px-4 py-2 rounded-xl text-black font-bold hover:text-blue-900 hover:bg-blue-200 cursor-pointer">
+            <span>SIGNIN</span>
+          </a>
+          <a href="register" className="ml-2 px-4 py-2 rounded-xl text-black font-bold hover:text-blue-900 hover:bg-blue-200 cursor-pointer">
+            <span>SIGNUP</span>
+          </a>
+        </div>
+      </div>
+    </div>
+  </nav>
 );
 
 export default Navbar;
