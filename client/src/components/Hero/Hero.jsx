a57586a34a36da5fd6ce96f962c4b763896e17ad
Pinaki Bhattacharjee <pinakipb2@gmail.com>
2021-10-13 08:48:45 +0530

    Added some texts ans icons

--- client/src/components/Hero/Hero.jsx
+++ client/src/components/Hero/Hero.jsx
@@ -5,14 +5,19 @@ const Hero = () => (
     <div className="container mx-auto flex px-5 py-24 md:flex-row flex-col items-center">
       <div className="lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center">
         <h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium text-gray-900">
-          Before they sold out
+          Keep Your Digital Identity
           <br className="hidden lg:inline-block" />
-          readymade gluten
+          Safe & Organised
         </h1>
-        <p className="mb-8 leading-relaxed">Copper mug try-hard pitchfork pour-over freegan heirloom neutra air plant cold-pressed tacos poke beard tote bag. Heirloom echo park mlkshk tote bag selvage hot chicken authentic tumeric truffaut hexagon try-hard chambray.</p>
+        <p className="mb-8 leading-relaxed">A one-stop platform for your Digital Identity to be Safe and Organized. This platform is made using Blockchain Technology which makes it Fast, Trustworthy and Immutable. </p>
         <div className="flex justify-center">
-          <button type="button" className="inline-flex text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded text-lg">Button</button>
-          <button type="button" className="ml-4 inline-flex text-gray-700 bg-gray-100 border-0 py-2 px-6 focus:outline-none hover:bg-gray-200 rounded text-lg">Button</button>
+          <button type="button" className="inline-flex text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded text-lg">Get Started</button>
+          <button type="button" className="ml-4 inline-flex text-gray-700 bg-gray-100 border-0 py-2 px-6 focus:outline-none hover:bg-gray-200 rounded text-lg">
+            <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6 mt-1 mr-2" viewBox="0 0 20 20" fill="currentColor">
+              <path fillRule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zM9.555 7.168A1 1 0 008 8v4a1 1 0 001.555.832l3-2a1 1 0 000-1.664l-3-2z" clipRule="evenodd" />
+            </svg>
+            Watch Video
+          </button>
         </div>
       </div>
       <div className="lg:max-w-lg lg:w-full md:w-1/2 w-5/6">
--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -9,7 +9,7 @@ const Navbar = () => (
         </div>
         <div className="flex-row">
           <a href="signin" className="ml-2 px-6 py-3 rounded-xl text-sm text-blue-600 border border-gray-400 font-bold cursor-pointer">
-            <span>SIGNIN</span>
+            <span>LOGIN</span>
           </a>
           <a href="register" className="ml-2 px-6 py-3 rounded-xl text-sm text-white bg-blue-600 border border-blue-500 font-bold cursor-pointer">
             <span>SIGNUP</span>
