37e8bb9e670603542e419f5c75fcad1f0c0211cd
Pinaki Bhattacharjee <pinakipb2@gmail.com>
2021-10-13 08:54:15 +0530

    Added some texts ans icons

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -8,10 +8,10 @@ const Navbar = () => (
           <img src="assets/nav_logo.png" alt="logo" className="h-16 ml-4" />
         </div>
         <div className="flex-row">
-          <a href="signin" className="ml-2 px-6 py-3 rounded-xl text-sm text-blue-600 border border-gray-400 font-bold cursor-pointer">
+          <a href="login" className="ml-2 px-6 py-3 rounded-xl text-sm text-blue-600 border border-gray-400 font-bold cursor-pointer">
             <span>LOGIN</span>
           </a>
-          <a href="register" className="ml-2 px-6 py-3 rounded-xl text-sm text-white bg-blue-600 border border-blue-500 font-bold cursor-pointer">
+          <a href="signup" className="ml-2 px-6 py-3 rounded-xl text-sm text-white bg-blue-600 border border-blue-500 font-bold cursor-pointer">
             <span>SIGNUP</span>
           </a>
         </div>
