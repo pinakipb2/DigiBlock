0fd75883035a1b5cd2ba0104a43532dffe30b7aa
grsakshi <sakshi.g642001@gmail.com>
2021-10-19 19:24:06 +0530

    added alerts and set up redux-thunk

--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -2,9 +2,10 @@
 
 import { createStore, applyMiddleware, compose } from 'redux';
 import logger from 'redux-logger';
+import thunk from "redux-thunk";
 import rootReducer from './root-reducer';
 
-const middlewares = [];
+const middlewares = [thunk];
 
 if (process.env.NODE_ENV === 'development') {
   middlewares.push(logger);
--- client/src/redux/user/user.actions.js
+++ client/src/redux/user/user.actions.js
@@ -1,3 +1,4 @@
+import { toast } from 'react-toastify';
 import UserActionTypes from './user.types';
 
 export const setWeb3 = (web3) => ({
@@ -5,16 +6,26 @@ export const setWeb3 = (web3) => ({
   payload: web3,
 });
 
-export const setCurrentUser = (account, balance, networkId) => ({
-  type: UserActionTypes.SET_CURRENT_USER,
-  payload: {
-    account,
-    balance,
-    networkId,
-  },
-});
+export const setCurrentUser = (account, balance, networkId) => (dispatch) => {
+  dispatch(
+    {
+      type: UserActionTypes.SET_CURRENT_USER,
+      payload: {
+        account,
+        balance,
+        networkId,
+      },
+    },
+  );
+  if (account) { toast.success('MetaMask wallet connected successfully'); }
+};
 
-export const setMetmaskInstalled = (status) => ({
-  type: UserActionTypes.CHECK_METAMASK_INSTALLED,
-  payload: status,
-});
+export const setMetmaskInstalled = (status) => (dispatch) => {
+  dispatch({
+    type: UserActionTypes.CHECK_METAMASK_INSTALLED,
+    payload: status,
+  });
+  if (!status) {
+    toast.warn('MetaMask not installed', { autoClose: 10000, toastId: 'Not-installed' });
+  }
+};
