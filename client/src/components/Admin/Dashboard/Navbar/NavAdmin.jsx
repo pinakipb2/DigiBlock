e1fc247d4b30b6d95d8d52fce00151c28f5a8e78
grsakshi <sakshi.g642001@gmail.com>
2021-11-12 19:22:04 +0530

    added pages in dashboard according to the selected field in side bar

--- client/src/components/Admin/Dashboard/Navbar/NavAdmin.jsx
+++ client/src/components/Admin/Dashboard/Navbar/NavAdmin.jsx
@@ -1,17 +1,13 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
 
 const NavAdmin = () => (
   <nav className="items-center">
     <div className="bg-white w-full px-4 py-1 flex justify-evenly z-50 border-b border-gray-300">
-      <div className="w-full flex justify-between sm:px-10 px-6 py-3 items-center">
+      <div className="w-full flex justify-between px-2 py-3.5 items-center">
         <div className="flex-row">
-          <Link to="/home" className="text-lg font-semibold cursor-pointer text-gray-500 hover:text-gray-900 pr-6">
-            <span>Home</span>
-          </Link>
-          <Link to="/contact" className="text-lg font-semibold cursor-pointer text-gray-500 hover:text-gray-900">
-            <span>Contact</span>
-          </Link>
+          <div className="cursor-pointer text-gray-500 hover:text-gray-900">
+            <i className="fas fa-bars fa-lg" />
+          </div>
         </div>
       </div>
     </div>
--- client/src/pages/Admin/Dashboard/AdminDashboard.jsx
+++ client/src/pages/Admin/Dashboard/AdminDashboard.jsx
@@ -1,16 +1,49 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { useLocation } from 'react-router-dom';
 import NavAdmin from '../../../components/Admin/Dashboard/Navbar/NavAdmin';
 import SideBar from '../../../components/Admin/Dashboard/SideBar/SideBar';
+import Stats from '../../../components/Admin/Dashboard/MainComponents/DashboardStats/Stats';
+import AdminProfile from '../../../components/Admin/Dashboard/MainComponents/Profile/Profile';
+import AdminDetails from '../../../components/Admin/Dashboard/MainComponents/Admins/AdminDetails';
 
-const AdminDashboard = () => (
-  <div className="flex bg-gray-200">
-    <div className="">
-      <SideBar />
+const AdminDashboard = () => {
+  const location = useLocation();
+  const [name, setName] = useState('Dashboard');
+  useEffect(() => {
+    const pathName = location.pathname.replace('/admin/', '');
+    setName(`${pathName.charAt(0).toUpperCase() + pathName.slice(1)}`);
+    return (() => {
+      setName('Dashboard');
+    });
+  }, [location]);
+
+  function renderInnerComponent() {
+    switch (name) {
+      case 'Dashboard':
+        return <Stats />;
+      case 'Admins':
+        return <AdminDetails />;
+      case 'Profile':
+        return <AdminProfile />;
+      default:
+        return <></>;
+    }
+  }
+
+  return (
+    <div className="flex bg-gray-200">
+      <div className="">
+        <SideBar />
+      </div>
+      <div className="flex-auto">
+        <NavAdmin />
+        <div className="text-3xl py-4 px-6 font-bold text-gray-700 font-roboto">{name}</div>
+        {
+         renderInnerComponent()
+        }
+      </div>
     </div>
-    <div className="flex-auto">
-      <NavAdmin />
-    </div>
-  </div>
-);
+  );
+};
 
 export default AdminDashboard;
