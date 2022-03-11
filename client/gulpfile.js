db8219bb44a778d3d3b71af0e21f77b088aa0cd7
Pinaki Bhattacharjee <pinakipb2@gmail.com>
2022-03-11 22:36:31 +0530

    Added gulp licenses for production build and created changelog.md

---
+++ client/gulpfile.js
@@ -0,0 +1,73 @@
+const gulp = require('gulp');
+const gap = require('gulp-append-prepend');
+
+const year = new Date().getFullYear();
+
+gulp.task('licenses', (done) => {
+  // this is to add DigiBlock licenses in the production mode for the minified js
+  gulp
+    .src('build/static/js/*chunk.js', { base: './' })
+    .pipe(
+      gap.prependText(`/*!
+
+=======================================================================
+* DigiBlock - v0.1.0
+=======================================================================
+
+* Copyright ${year} DigiBlock
+
+* Coded by Pinaki Bhattacharjee, Sakshi Gairola, Chandra Prakash
+
+=======================================================================
+
+* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+*/`)
+    )
+    .pipe(gulp.dest('./', { overwrite: true }));
+
+  // this is to add DigiBlock licenses in the production mode for the minified html
+  gulp
+    .src('build/index.html', { base: './' })
+    .pipe(
+      gap.prependText(`<!--
+
+=======================================================================
+* DigiBlock - v0.1.0
+=======================================================================
+
+* Copyright ${year} DigiBlock
+
+* Coded by Pinaki Bhattacharjee, Sakshi Gairola, Chandra Prakash
+
+=======================================================================
+
+* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+-->`)
+    )
+    .pipe(gulp.dest('./', { overwrite: true }));
+
+  // this is to add DigiBlock licenses in the production mode for the minified css
+  gulp
+    .src('build/static/css/*chunk.css', { base: './' })
+    .pipe(
+      gap.prependText(`/*!
+
+=======================================================================
+* DigiBlock - v0.1.0
+=======================================================================
+
+* Copyright ${year} DigiBlock
+
+* Coded by Pinaki Bhattacharjee, Sakshi Gairola, Chandra Prakash
+
+=======================================================================
+
+* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+*/`)
+    )
+    .pipe(gulp.dest('./', { overwrite: true }));
+  done();
+});
