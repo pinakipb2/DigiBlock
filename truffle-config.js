15b494bd678e172960b277fbab7142bebd34bf3d
Pinaki Bhattacharjee <pinakipb2@gmail.com>
2022-04-08 13:23:55 +0530

    Configured truffle-config

--- truffle-config.js
+++ truffle-config.js
@@ -1,37 +1,38 @@
-const path = require("path");
-// const HDWalletProvider = require("@truffle/hdwallet-provider");
+const path = require('path');
+require('dotenv').config();
+const HDWalletProvider = require('@truffle/hdwallet-provider');
 
 module.exports = {
   // See <http://truffleframework.com/docs/advanced/configuration>
   // to customize your Truffle configuration!
-  contracts_build_directory: path.join(__dirname, "client/src/contracts"),
+  contracts_build_directory: path.join(__dirname, 'client/src/contracts'),
   networks: {
     develop: {
-      port: 7545
+      port: 7545,
     },
-    test : {
-      host: "localhost",
+    test: {
+      host: 'localhost',
       port: 8545,
-      network_id: "*"
+      network_id: '*',
     },
-    // rinkeby: {
-    //   provider: () => new HDWalletProvider(mnemonic, `https://rinkeby.infura.io/v3/`),
-    //   network_id: 4,       // Ropsten's id
-    //   gas: 5500000,        // Ropsten has a lower block limit than mainnet
-    //   confirmations: 2,    // # of confs to wait between deployments. (default: 0)
-    //   timeoutBlocks: 200,  // # of blocks before a deployment times out  (minimum/default: 50)
-    //   skipDryRun: true     // Skip dry run before migrations? (default: false for public nets )
-    // }
+    rinkeby: {
+      provider: () => new HDWalletProvider(process.env.MNEMONIC, `wss://rinkeby.infura.io/ws/v3/${process.env.RINKEBY_INFURA_API_KEY}`),
+      network_id: 4, // Rinkeby's id
+      gas: 5500000, // Rinkeby has a lower block limit than mainnet
+      confirmations: 2, // # of confs to wait between deployments. (default: 0)
+      timeoutBlocks: 200, // # of blocks before a deployment times out  (minimum/default: 50)
+      skipDryRun: true, // Skip dry run before migrations? (default: false for public nets )
+    },
   },
   compilers: {
     solc: {
-      version: "0.8.10",
+      version: '0.8.10',
       settings: {
         optimizer: {
           enabled: true,
           runs: 1000,
         },
       },
-    }
-  }
+    },
+  },
 };
